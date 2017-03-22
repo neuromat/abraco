4eee7c53c7711b3b0ecd3ef4f7c54d04b3c94da6
Sidneia França <sidneiafranca@gmail.com>
2017-03-22 10:19:36 -0300

    ajuste js do slider

--- website/static/js/slider.js
+++ website/static/js/slider.js
@@ -1,10 +1,7 @@
-// Instantiate the Bootstrap carousel
 $('.multi-item-carousel').carousel({
   interval: false
 });
 
-// for every slide in carousel, copy the next slide's item in the slide.
-// Do the same for the next, next item.
 $('.multi-item-carousel .item').each(function(){
   var next = $(this).next();
   if (!next.length) {

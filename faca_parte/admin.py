a6a5fb6d5cc7e75e06eb551fcd8281115c450ac6
carlosribas <caduribas@gmail.com>
2017-04-11 10:49:58 -0300

    Tela de pessoa dentro de admin foi customizada

--- faca_parte/admin.py
+++ faca_parte/admin.py
@@ -3,5 +3,17 @@ from models import Interest, Person, Schooling
 
 
 admin.site.register(Interest)
-admin.site.register(Person)
 admin.site.register(Schooling)
+
+class PersonAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ['full_name', 'email', 'birth', 'schooling', 'zipcode', 'street', 'district', 'city', 'state',
+                       'select_interest', 'note']
+        }),
+    )
+    search_fields = ['full_name', 'email']
+    list_display = ('full_name', 'email', 'select_interest')
+    list_display_links = ('full_name', )
+
+admin.site.register(Person, PersonAdmin)

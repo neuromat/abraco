377a6b05715b0300a1e17a325bec561e9b0dadec
neiasf <sidneiasfranca@gmail.com>
2017-06-06 11:19:50 -0300

    aumentando o intervalo de transicoes de slides do item novidades da pagina principal

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,5 +1,5 @@
 $('.multi-item-carousel').carousel({
-  interval: 5000
+  interval: 15000
 });
 $('.multi-item-carousel .carousel-inner .item').each(function(){
   if($(window).width() > 1000){
@@ -8,7 +8,6 @@ $('.multi-item-carousel .carousel-inner .item').each(function(){
       next = $(this).siblings(':first');
     }
     next.children(':first-child').clone().appendTo($(this));
-
     if (next.next().length>0) {
       next.next().children(':first-child').clone().appendTo($(this));
     } else {

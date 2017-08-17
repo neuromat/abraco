9671e14b403751405ac06b7fbc7e2e6e39f95a9d
Lucas Helfstein <lucashelfstein@gmail.com>
2017-08-17 10:38:31 -0300

    Aldryn-newsblog configs

--- website/settings.py
+++ website/settings.py
@@ -130,10 +130,13 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'homemade.db'),
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+    }
 }
 
 

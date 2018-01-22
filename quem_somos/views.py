91a3554f61228a86b4fcc375f72208efc63342eb
carlosribas <caduribas@gmail.com>
2018-01-22 11:25:43 -0200

    Ordenar pessoas pelo nome

--- quem_somos/views.py
+++ quem_somos/views.py
@@ -4,7 +4,7 @@ from models import Person
 
 
 def who_we_are(request, template_name="quem_somos.html"):
-    people = Person.objects.all()
+    people = Person.objects.all().order_by('name')
     coordination = people.filter(coordination=True)
     members = people.filter(coordination=False)
     context = {'coordination': coordination, 'members': members}
